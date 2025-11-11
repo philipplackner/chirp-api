f569e0888ce6f6cec34d1d1e0025b146e06f08ce
Kai Cilliers <kaicilliers@protonmail.com>
2025-11-11 09:37:19 +0200

    Minor fixes

--- user/src/main/kotlin/com/plcoding/chirp/infra/security/PasswordEncoder.kt
+++ user/src/main/kotlin/com/plcoding/chirp/infra/security/PasswordEncoder.kt
@@ -8,7 +8,7 @@ class PasswordEncoder {
 
     private val bcrypt = BCryptPasswordEncoder()
 
-    fun encode(rawPassword: String): String = bcrypt.encode(rawPassword)
+    fun encode(rawPassword: String): String? = bcrypt.encode(rawPassword)
 
     fun matches(rawPassword: String, hashedPassword: String): Boolean {
         return bcrypt.matches(rawPassword, hashedPassword)
